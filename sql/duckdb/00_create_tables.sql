b7a9e9a2debc98ea8870cb7621eb1b87d9215fed
affirm-bats-yodel <affirm-bats-yodel@duck.com>
2024-10-27 06:30:43 +0000

    chore: add `IF NOT EXISTS` clauses

--- sql/duckdb/00_create_tables.sql
+++ sql/duckdb/00_create_tables.sql
@@ -1,12 +1,12 @@
 -- List of Buses
-CREATE TABLE bus_lists (
+CREATE TABLE IF NOT EXISTS bus_lists (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     create_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
     name VARCHAR UNIQUE NOT NULL
 );
 
 -- a Timetable of buses
-CREATE TABLE bus_timetables (
+CREATE TABLE IF NOT EXISTS bus_timetables (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     bus_id UUID REFERENCES bus_lists(id), -- reference from bus_lists.id
     stop_name VARCHAR NOT NULL, -- bus stop name
